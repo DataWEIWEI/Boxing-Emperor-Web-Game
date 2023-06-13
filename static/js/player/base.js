ae89b4d298fa221196f58011f9affeadf486213f
Wei Wei <2312052806@qq.com>
2023-06-13 08:32:00 +0800

    attack and be attacked action

--- static/js/player/base.js
+++ static/js/player/base.js
@@ -28,6 +28,8 @@ export class Player extends GameObeject {
         this.status = 3; // 0: idle, 1: forward, 2: backward, 3: jump, 4: attack,  5: be attacked
         this.animation = new Map();
         this.frame_current_cnt = 0;
+
+        this.hp = 100;
     }
 
     start() {
@@ -79,6 +81,8 @@ export class Player extends GameObeject {
     }
 
     update_direction() {
+        if (this.status === 6) return;
+
         let players = this.root.players;
         if (players[0] && players[1]) {
             let me = this, you = players[1 - this.id];
@@ -88,9 +92,7 @@ export class Player extends GameObeject {
     }
 
     update_move() {
-        if (this.status === 3) {
-            this.vy += this.gravity;
-        }
+        this.vy += this.gravity;
 
         this.x += this.vx * this.timedelta / 1000;
         this.y += this.vy * this.timedelta / 1000;
@@ -98,7 +100,8 @@ export class Player extends GameObeject {
         if (this.y > 450) {
             this.y = 450;
             this.vy = 0;
-            this.status = 0;
+            
+            if (this.status === 3) this.status = 0;
         }
 
         if (this.x < 0)  {
@@ -108,17 +111,87 @@ export class Player extends GameObeject {
         }
     }
 
+    is_collision(r1, r2) {
+        if (Math.max(r1.x1, r2.x1) > Math.min(r1.x2, r2.x2)) {
+            return false;
+        }
+        if (Math.max(r1.y1, r2.y1) > Math.min(r1.y2, r2.y2)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    is_attack() {
+        if (this.status === 6) return;
+
+        this.status = 5;
+        this.frame_current_cnt = 0;
+        
+        this.hp = Math.max(this.hp - 50, 0);
+
+        if (this.hp <= 0) {
+            this.status = 6;
+            this.frame_current_cnt = 0;
+        }
+    }
+
+    update_attack() {
+        if (this.status === 4 && this.frame_current_cnt === 18) {
+            let me = this, you = this.root.players[1 - this.id];
+            let r1;
+            if (this.direction > 0) {
+                r1 = {
+                    x1: me.x + 120,
+                    y1: me.y + 40,
+                    x2: me.x + 120 + 105,
+                    y2: me.y + 40 + 20,
+                }
+            } else {
+                r1 = {
+                    x1: me.x + me.width - 125 - 100,
+                    y1: me.y + 40,
+                    x2: me.x + me.width - 125 - 100 + 105,
+                    y2: me.y + 40 + 20,
+                }
+            }
+
+            let r2 = {
+                x1: you.x,
+                y1: you.y,
+                x2: you.x + you.width,
+                y2: you.y + you.height,
+            }
+
+            if (this.is_collision(r1, r2)) {
+                you.is_attack();
+            }
+        }
+    }
+
     update() {
         this.update_control();
         this.update_move();
         this.update_direction();
+        this.update_attack();
 
         this.render();
     }
 
     render() {
         // this.ctx.fillStyle = this.color;
-        // this.ctx.fillRect(this.x, this.y, this.width, this.height)a
+        // this.ctx.fillRect(this.x, this.y, this.width, this.height)；
+
+        // this.ctx.fillStyle = 'blue';
+        // this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        // if (this.direction > 0) {
+        //     this.ctx.fillStyle = 'green';
+        //     this.ctx.fillRect(this.x + 120, this.y + 40, 105, 20);
+        // } else {
+        //     this.ctx.fillStyle = 'green';
+        //     this.ctx.fillRect(this.x + this.width - 125 - 100, this.y + 40, 105, 20);
+        // }
 
         let status = this.status;
         let obj = this.animation.get(status);
@@ -141,8 +214,12 @@ export class Player extends GameObeject {
             }
         }
 
-        if ((status === 4 || status === 3) && this.frame_current_cnt === obj.frame_rate * (obj.frame_cnt - 1)) {
-            this.status = 0; // end of animation
+        if (status <= 6 && status >= 3 && this.frame_current_cnt === obj.frame_rate * (obj.frame_cnt - 1) - 1) {
+            if (status === 6) {
+                this.frame_current_cnt--;
+            } else {
+                this.status = 0; // end of animation
+            }
         }
 
         this.frame_current_cnt++;
